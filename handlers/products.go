335ddaf16eb9489fcf089c4a2f07a5ae754fea91
dhg <duyhung8a2@gmail.com>
2024-06-15 13:46:24 +0700

    feat: restful api

--- handlers/products.go
+++ handlers/products.go
@@ -14,7 +14,17 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return 
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
