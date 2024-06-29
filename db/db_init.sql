682f6c08e3974dc4762e8abc2e6dc84d6e65f9f5
dhg <duyhung8a2@gmail.com>
2024-06-29 22:26:54 +0700

    feat: clean up project, start fresh

--- db/db_init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `user` (username, email, password) VALUES ('duyhung', 'duyhung@gmail.com', 'duyhung');
-INSERT INTO `page` (user_id, title) VALUES (1, 'First page');
-INSERT INTO `page` (user_id, title, parent_id) VALUES (2, 'Second Sub Page', 1);
--- main.go
+++ main.go
@@ -2,33 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"remember_them/handlers"
 	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
-	chiMiddleware "github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/httprate"
-	redocMiddleware "github.com/go-openapi/runtime/middleware"
 	"github.com/knadh/koanf/parsers/dotenv"
 	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
 )
-
-// // Init database
-// bob, err := db.ConnectDB()
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// defer bob.Close()
-// db.InitDB(bob)
 
 var k = koanf.New(".")
 
@@ -47,35 +33,6 @@ func main() {
 	log.Printf("Server is running on port %s", port)
 
 	r := chi.NewRouter()
-
-	r.Use(chiMiddleware.RequestID)
-	r.Use(chiMiddleware.RealIP)
-	r.Use(chiMiddleware.Logger)
-	r.Use(chiMiddleware.Recoverer)
-	r.Use(chiMiddleware.Compress(5))
-	r.Use(chiMiddleware.Timeout(60 * time.Second))
-	r.Use(httprate.Limit(10, 1*time.Minute,
-		httprate.WithLimitHandler(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusTooManyRequests)
-			json.NewEncoder(w).Encode(map[string]string{
-				"message": "Too many request, please try again later",
-			})
-		})))
-
-	// Index handler
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
-	})
-
-	ph := handlers.NewProducts(l)
-	r.Mount("/products", ph.Routes())
-
-	options := redocMiddleware.RedocOpts{SpecURL: "/swagger.yaml"}
-	r.Mount("/docs", redocMiddleware.Redoc(options, nil))
-
-	workDir, _ := os.Getwd()
-	r.Mount("/swagger.yaml", http.FileServer(http.Dir(filepath.Join(workDir, "./"))))
 
 	s := &http.Server{
 		Addr:         port,
